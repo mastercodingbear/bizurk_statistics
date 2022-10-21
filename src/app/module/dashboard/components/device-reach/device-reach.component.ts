b7787bf34ae6180a5e08878ec056cc105355934f
Blue Lotus <mastercodingbear@gmail.com>
2022-10-21 12:33:36 -0500

    alpha change

--- src/app/module/dashboard/components/device-reach/device-reach.component.ts
+++ src/app/module/dashboard/components/device-reach/device-reach.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ApexOptions } from 'ng-apexcharts';
 
 @Component({
   selector: 'app-device-reach',
   templateUrl: './device-reach.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./device-reach.component.scss'],
 })
 export class DeviceReachComponent implements OnInit {
@@ -11,35 +12,55 @@ export class DeviceReachComponent implements OnInit {
   constructor() {
     this.chartOptions = {
       title: {
-        text: 'Gender / Age',
+        text: 'Reach by device',
         align: 'left',
         style: {
           fontSize: '20px',
           fontWeight: 'bold',
           fontFamily: undefined,
         },
+        offsetX: 24,
+        offsetY: 20,
       },
       chart: {
         animations: {
           speed: 400,
-          animateGradually: {
-            enabled: false,
-          },
         },
         height: '100%',
         type: 'pie',
+        events: {
+          animationEnd: function (ctx: any, opts: any) {
+            console.log(ctx);
+            console.log(opts);
+            ctx.toggleDataPointSelection(0, 1);
+          },
+        },
       },
       colors: ['#E786D7', '#7F7FD5'],
       dataLabels: {
         enabled: true,
-        textAnchor: 'middle',
+        textAnchor: 'start',
+        style: {
+          fontSize: '18px',
+        },
+        offsetX: 10,
+        offsetY: 10,
+        dropShadow: {
+          enabled: false,
+        },
+        formatter: function (val: number) {
+          return `${val}%`;
+        },
       },
       labels: ['Desktop', 'Mobile'],
       plotOptions: {
         pie: {
           startAngle: 40,
           endAngle: 400,
+          customScale: 0.75,
           expandOnClick: true,
+          offsetY: 20,
+          offsetX: -30,
         },
       },
       states: {
@@ -56,9 +77,25 @@ export class DeviceReachComponent implements OnInit {
       },
       series: [14, 86],
       legend: {
-        position: 'top',
-        horizontalAlign: 'right',
-        offsetX: 20,
+        position: 'right',
+        floating: true,
+        offsetY: 60,
+      },
+      yaxis: {
+        crosshairs: {
+          show: true,
+          position: 'back',
+          stroke: {
+            color: '#b6b6b6',
+            width: 1,
+            dashArray: 0,
+          },
+        },
+      },
+      stroke: {
+        show: true,
+        width: 10,
+        colors: '#FDFDFF',
       },
     };
   }
